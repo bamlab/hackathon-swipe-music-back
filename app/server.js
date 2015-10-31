d4a530ffeea80f2faaac546c8e19e6287d2c8886
Nicolas Djambazian <nicolas@djambazian.fr>
2015-10-31 14:28:55 +0100

    Add api doc

--- app/server.js
+++ app/server.js
@@ -1,7 +1,15 @@
 var Hapi = require('hapi');
 
+// plugins
+var vision = require('vision');
+var inert = require('inert');
+var lout = require('lout');
+
+
 var server = new Hapi.Server();
 server.connection({ port: 3000 });
+
+server.register([ vision, inert, { register: lout }], function(err) {});
 
 server.route({
   method: 'GET',
